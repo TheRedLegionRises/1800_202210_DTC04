6881d6c5e66e5b2d169e8e9804d143ad3c6c0a17
Steven Chow <steven.chow@live.ca>
2022-03-04 20:10:01 -0800

    update landing page

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -3,7 +3,8 @@
 // (navbar, footer, and other things) into html doc. 
 //---------------------------------------------------
 function loadSkeleton(){
-    console.log($('#navbarPlaceholder').load('./header.html'));
+    console.log($('#nav_main').load('./header_main.html'));
+    console.log($('#nav_index').load('./header_index.html'));
     console.log($('#footerPlaceholder').load('./footer.html'));
 }
 loadSkeleton();  //invoke the function
