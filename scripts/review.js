456a29c3249ab7ad095ea78ad1e71a593c38969a
Neil Cao <caofangxin7@gmail.com>
2022-03-24 22:24:49 -0700

    add user uid to review collection

--- scripts/review.js
+++ scripts/review.js
@@ -1,14 +1,24 @@
 function writeReview() {
-  var currentRestaurant = db.collection("reviews");
-
-  currentRestaurant
-    .add({
-      Title: document.getElementById("title").value, //get the value of the field with id="nameInput"
-      Level: document.getElementById("level").value, //get the value of the field with id="schoolInput"
-      Price: document.getElementById("price").value,
-      Description: document.getElementById("description").value,
-    })
-    .then(() => {
-      window.location.href = "main.html";
-    });
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      var userID = user.uid;
+      currentUser.get().then((userDoc) => {
+        var userEmail = userDoc.data().email;
+        var currentRestaurant = db.collection("reviews");
+        currentRestaurant
+          .add({
+            UserID: userID,
+            UserEmail: userEmail,
+            Title: document.getElementById("title").value,
+            Level: document.getElementById("level").value,
+            Price: document.getElementById("price").value,
+            Description: document.getElementById("description").value,
+          })
+          .then(() => {
+            window.location.href = "main.html";
+          });
+      });
+    }
+  });
 }
