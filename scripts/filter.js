4067c147bfd40b871f86237bc8d268c3028ce893
TheRedLegionRises <65699655+TheRedLegionRises@users.noreply.github.com>
2022-03-31 19:00:24 -0700

    added populate stuff:

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,4 +1,4 @@
-function write_filters(){
+function write_filters() {
     var filtersRef = db.collection("Filters");
 
     filtersRef.add({
@@ -12,7 +12,7 @@ function write_filters(){
     filtersRef.add({
         code: "HMBRGR",
         filterName: "Hamburgers",
-        cuisineType:"Western",
+        cuisineType: "Western",
         selected: 0,
 
     });
@@ -25,13 +25,13 @@ function write_filters(){
 
     });
 
-    
+
 }
 
 function displayFilters() {
     let filterOptionsTemplate = document.getElementById("filterOptionsTemplate");
     let filterOptionsTab = document.getElementById("filter_options_tab");
-    
+
     db.collection("Filters").get()
         .then(allFilters => {
             allFilters.forEach(doc => {
@@ -41,7 +41,7 @@ function displayFilters() {
                 // console.log(test)
                 // var test1 = !test;
 
-                
+
                 //console.log(filterCode)
                 // var hikeLength = doc.data().length;
 
@@ -54,7 +54,7 @@ function displayFilters() {
                 //testFilterButton.querySelector(".filter_button").onclick = () => displaySelected(filterName);
                 filterOptionsTab.appendChild(testFilterButton);
 
-                
+
             })
 
         })
@@ -62,7 +62,7 @@ function displayFilters() {
 
 displayFilters();
 
-function displaySelected(filterName){
+function displaySelected(filterName) {
     $(".selected_filter_button").remove();
 
     let filterSelectedTemplate = document.getElementById("selectedOptionsTemplate");
@@ -76,7 +76,7 @@ function displaySelected(filterName){
     //         selectFilterButton.querySelector(".selected_filter_button").innerHTML = filterName;
     //         //selectFilterButton.querySelector(".filter_button").onclick = () =>
     //         filterSelectedTab.appendChild(selectFilterButton)
-            
+
     //     })
     // })
 
@@ -88,7 +88,7 @@ function displaySelected(filterName){
 }
 
 // Not really sure what this is used for yet, prob gonna add some stuff later
-function addTimesUsed(filterCode, filterName) {  
+function addTimesUsed(filterCode, filterName) {
     console.log("inside");
 
     // Asks database hey, is there any in the "Filters" document that as a field "code" that matches the value filterCode
@@ -96,73 +96,93 @@ function addTimesUsed(filterCode, filterName) {
     // Filters = queryFilter.docs makes list(?) of docs
     // Filters[0] returns first doc that matches the requirement, Filters[1] for second, etc etc.
     db.collection("Filters").where("code", "==", filterCode)
-    .get()
-    .then(queryFilter => {
-        //see how many results you have got from the query
-        size = queryFilter.size;
-        // get the documents of query
-        Filters = queryFilter.docs;
+        .get()
+        .then(queryFilter => {
+            //see how many results you have got from the query
+            size = queryFilter.size;
+            // get the documents of query
+            Filters = queryFilter.docs;
 
-        // test
-        if (size == 1) {
-            id = Filters[0].id;
-            var select = Filters[0].data().selected;
-            
-            // Not too sure, had an idea but then went nowhere
-            if (select == 0){
-                select += 1;
+            // test
+            if (size == 1) {
+                id = Filters[0].id;
+                var select = Filters[0].data().selected;
+
+                // Not too sure, had an idea but then went nowhere
+                if (select == 0) {
+                    select += 1;
+                }
+
+
+                else {
+                    select -= 1;
+                }
+
+                db.collection("Filters").doc(id).update({
+                    //Firebase documentation has this method for incrementation.
+                    // scores: firebase.firestore.FieldValue.increment(1)
+                    //timesUsed: firebase.firestore.FieldValue.increment(1)
+                    selected: select
+
+                })
+
+            } else {
+                console.log("Query has more than one data")
             }
-            
-
-            else{
-                select -= 1;
-            }
-
-            db.collection("Filters").doc(id).update({
-                //Firebase documentation has this method for incrementation.
-                // scores: firebase.firestore.FieldValue.increment(1)
-                //timesUsed: firebase.firestore.FieldValue.increment(1)
-                selected: select
-
-            })
-
-        } else {
-            console.log("Query has more than one data")
-        }
-    })
-    .catch((error) => {
-        console.log("Error getting documents: ", error);
-    });
+        })
+        .catch((error) => {
+            console.log("Error getting documents: ", error);
+        });
 
     // Functions to display the chosen filter button and display the restaurants that got filtered
     displaySelected(filterName);
     displayRestaurants(filterCode);
 }
 
-function displayRestaurants(filterCode){
-    $(".restaurantNames").remove();
+function displayRestaurants(filterCode) {
+    let RestaurantCard = document.getElementById("RestaurantCard");
+    let RestaurantCardGroup = document.getElementById("restaurant_suggestions");
+    //$(".restaurantNames").remove();
+    $("#restaurant_suggestions").empty();
 
     db.collection("restaurant").where("filter", "==", filterCode)
-    .get()
-    .then(queryFilter => {
-        //see how many results you have got from the query
-        size = queryFilter.size;
-        console.log(size);
-        // get the documents of query
-        restaurants = queryFilter.docs;
-                
-        for (i = 0; i < size; i++){
-            restaurantName = restaurants[i].data().name;
-            console.log(restaurantName);
-            $("#restaurant_suggestions").append("<p class='restaurantNames'>" + restaurantName + "</p>");
-        }
-})}
+        .get()
+        .then(queryFilter => {
+            //see how many results you have got from the query
+            size = queryFilter.size;
+            console.log(size);
+            // get the documents of query
+            restaurants = queryFilter.docs;
 
-function hide(){
+            for (i = 0; i < size; i++) {
+                var restaurantName = restaurants[i].data().name;
+                //console.log(restaurantName);
+                //$("#restaurant_suggestions").append("<p class='restaurantNames'>" + restaurantName + "</p>");
+
+                var RestaurantID = restaurants[i].data().id;
+                var RestaurantPrice = restaurants[i].data().price;
+                var RestaurantDescription = restaurants[i].data().description;
+
+                let newRestaurantCard = RestaurantCard.content.cloneNode(true);
+                newRestaurantCard.querySelector(".card-title").innerHTML = restaurantName;
+                newRestaurantCard.querySelector(".price").innerHTML = RestaurantPrice;
+                newRestaurantCard.querySelector(".card-text").innerHTML = RestaurantDescription;
+                //newRestaurantCard.querySelector("a").onclick = () => setRestuarantData(RestaurantID);
+
+                newRestaurantCard.querySelector(
+                    "img"
+                ).src = `./images/${RestaurantID}.jpg`;
+
+                RestaurantCardGroup.appendChild(newRestaurantCard);
+            }
+        })
+}
+
+function hide() {
     $(this).remove();
 }
 
-function setup(){
+function setup() {
     console.log("Start Setup");
     $("body").on("click", ".selected_filter_button", hide);
 
