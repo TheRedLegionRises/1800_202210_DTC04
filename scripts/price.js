3fef4d5055fa3c5129f865dd7c6dab8160378b0f
TheRedLegionRises <65699655+TheRedLegionRises@users.noreply.github.com>
2022-04-06 15:24:56 -0700

    Added show/hide for sort by price

--- scripts/price.js
+++ scripts/price.js
@@ -56,18 +56,35 @@ function sort_by_price_high_to_low_restaurant() {
     .get()
         .then(AllRestaurants => {
             AllRestaurants.forEach(doc => {
-                var RestaurantName = doc.data().name
-                var RestaurantID = doc.data().id
-                var RestaurantPrice = doc.data().price
-                var RestaurantDescription = doc.data().description
+                var RestaurantName = doc.data().name;
+                var RestaurantID = doc.data().id;
+                var RestaurantPrice = doc.data().price;
+                var RestaurantDescription = doc.data().description;
 
-                let newRestaurantCard = RestaurantCard.content.cloneNode(true)
-                newRestaurantCard.querySelector('.card-title').innerHTML = RestaurantName
-                newRestaurantCard.querySelector('.price').innerHTML = RestaurantPrice
-                newRestaurantCard.querySelector('.card-text').innerHTML = RestaurantDescription
-                newRestaurantCard.querySelector("img").src = `./images/${RestaurantID}.jpg`
-                RestaurantCardGroup.appendChild(newRestaurantCard)
+                let newRestaurantCard = RestaurantCard.content.cloneNode(true);
+                newRestaurantCard.querySelector('.card-title').innerHTML = RestaurantName;
+                newRestaurantCard.querySelector('.price').innerHTML = RestaurantPrice;
+                newRestaurantCard.querySelector('.card-text').innerHTML = RestaurantDescription;
+                // hides description first and sets ID of the description box to restaurantID
+                newRestaurantCard.querySelector(".card-text").style.display = "none";
+                newRestaurantCard.querySelector(".card-text").setAttribute("id", `${RestaurantID}`);
+            
+                // shows/hides the description
+                newRestaurantCard.querySelector(".show").onclick = () => showDescription(RestaurantID);
+
+                newRestaurantCard.querySelector("img").src = `./images/${RestaurantID}.jpg`;
+                RestaurantCardGroup.appendChild(newRestaurantCard);
 
             })
         })
 }
+
+function showDescription(id){
+    var x = document.getElementById(`${id}`);
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+  
