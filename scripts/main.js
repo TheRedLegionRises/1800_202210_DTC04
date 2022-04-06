4eda52d1928b8611edced369a8c414d4953ad3b7
Neil Cao <caofangxin7@gmail.com>
2022-04-06 13:58:16 -0700

    add restaurant

--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,25 @@
 // Store user preference as global variable
 
-var eater_type
-var cuisine_type
+var eater_type;
+var cuisine_type;
 
 // Grab user preferences and populate main page
 function user_preference() {
-  firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-          currentUser = db.collection("users").doc(user.uid)
-          currentUser.get()
-              .then((doc) => {
-                  eater_type = doc.data().eater_type
-                  cuisine_type = doc.data().cuisine_type
-                  console.log("GET USER PREF " + cuisine_type + eater_type)
-                  if (eater_type.length > 0 || cuisine_type.length > 0) {
-                    populate_preference_restaurants()
-                  } else {
-                    populate_restaurants()
-                  }
-              })
-      }
-  })
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      currentUser = db.collection("users").doc(user.uid);
+      currentUser.get().then((doc) => {
+        eater_type = doc.data().eater_type;
+        cuisine_type = doc.data().cuisine_type;
+        console.log("GET USER PREF " + cuisine_type + eater_type);
+        if (eater_type.length > 0 || cuisine_type.length > 0) {
+          populate_preference_restaurants();
+        } else {
+          populate_restaurants();
+        }
+      });
+    }
+  });
 }
 
 function populate_restaurants() {
@@ -52,14 +51,12 @@ function populate_restaurants() {
         RestaurantCardGroup.appendChild(newRestaurantCard);
       });
     });
-    console.log("populate_restaurants() ran successfully")
+  console.log("populate_restaurants() ran successfully");
 }
-
 
 function setRestuarantData(id) {
   localStorage.setItem("RestaurantID", id);
 }
-
 
 // populate restaurant cards with preference
 
@@ -67,27 +64,37 @@ function populate_preference_restaurants() {
   let RestaurantCard = document.getElementById("RestaurantCard");
   let RestaurantCardGroup = document.getElementById("RestaurantCardGroup");
   db.collection("restaurant")
-      .get()
-      .then((AllRestaurants) => {
-          AllRestaurants.forEach((doc) => {
-              restaurant_eater_type = doc.data().eater_type
-              restaurant_cuisine_type = doc.data().cuisine_type
-              if (eater_type.some(eater => restaurant_eater_type.includes(eater)) && cuisine_type.some(cuisine => restaurant_cuisine_type.includes(cuisine))) {
-                  var RestaurantName = doc.data().name;
-                  var RestaurantID = doc.data().id;
-                  var RestaurantPrice = doc.data().price;
-                  var RestaurantDescription = doc.data().description;
-                  let newRestaurantCard = RestaurantCard.content.cloneNode(true);
-                  newRestaurantCard.querySelector(".card-title").innerHTML = RestaurantName;
-                  newRestaurantCard.querySelector(".price").innerHTML = RestaurantPrice;
-                  newRestaurantCard.querySelector(".card-text").innerHTML = RestaurantDescription;
-                  newRestaurantCard.querySelector("a").onclick = () =>setRestuarantData(RestaurantID);
-                  newRestaurantCard.querySelector("img").src = `./images/${RestaurantID}.jpg`;
-                  RestaurantCardGroup.appendChild(newRestaurantCard);
-              }
-          });
+    .get()
+    .then((AllRestaurants) => {
+      AllRestaurants.forEach((doc) => {
+        restaurant_eater_type = doc.data().eater_type;
+        restaurant_cuisine_type = doc.data().cuisine_type;
+        if (
+          eater_type.some((eater) => restaurant_eater_type.includes(eater)) &&
+          cuisine_type.some((cuisine) =>
+            restaurant_cuisine_type.includes(cuisine)
+          )
+        ) {
+          var RestaurantName = doc.data().name;
+          var RestaurantID = doc.data().id;
+          var RestaurantPrice = doc.data().price;
+          var RestaurantDescription = doc.data().description;
+          let newRestaurantCard = RestaurantCard.content.cloneNode(true);
+          newRestaurantCard.querySelector(".card-title").innerHTML =
+            RestaurantName;
+          newRestaurantCard.querySelector(".price").innerHTML = RestaurantPrice;
+          newRestaurantCard.querySelector(".card-text").innerHTML =
+            RestaurantDescription;
+          newRestaurantCard.querySelector("a").onclick = () =>
+            setRestuarantData(RestaurantID);
+          newRestaurantCard.querySelector(
+            "img"
+          ).src = `./images/${RestaurantID}.jpg`;
+          RestaurantCardGroup.appendChild(newRestaurantCard);
+        }
       });
-      console.log("populate_preference_restaurants() ran successfully")
+    });
+  console.log("populate_preference_restaurants() ran successfully");
 }
 
 function writeRestaurants() {
@@ -95,18 +102,23 @@ function writeRestaurants() {
   var restaurantRef = db.collection("restaurant");
 
   restaurantRef.add({
-    id: "r_00",
-    name: "McDonald's",
+    id: "r_05",
+    name: "Kosoo Korean Restaurant Chicken & BBQ",
     city: "Vancouver",
     province: "BC",
-    review: "4.5/5",
-    price: "$",
+    review: "4.2/5",
+    price: "$$",
+    eater_type: ["Omnivore"],
+    cuisine_type: ["Korean"],
+    filter: "KOREAN",
+    search: "korean",
+    url: "https://kosoo.ca/",
     description:
-      "McDonald's is an American multinational fast food corporation, founded in 1940",
+      "Streamlined Korean establishment serving fried chicken, pork specialties & BBQ dishes, plus soups.",
   });
   restaurantRef.add({
-    id: "r_01",
-    name: "A&W",
+    id: "r_06",
+    name: "CinCin Ristorante + Bar",
     city: "Vancouver",
     province: "BC",
     review: "4.5/5",
@@ -115,7 +127,7 @@ function writeRestaurants() {
       "A&W Restaurants is an American chain of fast-food restaurants distinguished by its burgers, draft root beer and root beer floats.",
   });
   restaurantRef.add({
-    id: "r_02",
+    id: "r_07",
     name: "Burger King",
     city: "Vancouver",
     province: "BC",
@@ -126,5 +138,6 @@ function writeRestaurants() {
   });
 }
 
-user_preference()
-.then(console.log("OUTSIDE OF FUNCTIONS" + cuisine_type + eater_type))
+user_preference().then(
+  console.log("OUTSIDE OF FUNCTIONS" + cuisine_type + eater_type)
+);
